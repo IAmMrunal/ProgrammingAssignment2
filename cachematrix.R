07ca908a93f94d0a64a4f6ad2355936443fa1fef
IAmMrunal <mrunaldivecha@gmail.com>
2021-02-25 18:00:17 +0530

    Added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,33 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+##Caching the Inverse of a Matrix
 
-## Write a short comment describing this function
+## 'makeCacheMatrix' creates a special "matrix" object that can cache its inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  i <- NULL
-  set <- function(y) {
+  i <- NULL                               # initialize object    
+  set <- function(y) {                    
     x <<- y
     i <<- NULL
   }
-  get <- function() x
-  setinv <- function(inv) i <<- inv
-  getinv <- function() i
+  get <- function() x                     # function to return matrix
+  setinv <- function(inv) i <<- inv       # function to set inverse of matrix
+  getinv <- function() i                  # function to return inverse of matrix
   list(set = set, get = get,
        setinv = setinv,
-       getinv = getinv)
+       getinv = getinv)                   # return list of functions
 }
 
 
-## Write a short comment describing this function
+## 'cacheSolve' solves and returns the inverse of a matrix unless the inverse is already
+## in cache in which case it will return the inverse from the cache
 
 cacheSolve <- function(x, ...) {
-  i <- x$getinv()
-  if(!is.null(i)) {
+  i <- x$getinv()                         # get the inverse from cache
+  if(!is.null(i)) {                       # check if the inverse exists in cache
     message("getting cached data")
-    return(i)
+    return(i)                             # return inverse from cache
   }
-  data <- x$get()
-  i <- solve(data, ...)
-  x$setinv(i)
-  i
+  data <- x$get()                         # get the matrix from cache
+  i <- solve(data, ...)                   # calculate the inverse of the matrix
+  x$setinv(i)                             # store the inverse in cache
+  i                                       # return calculated inverse
 }
